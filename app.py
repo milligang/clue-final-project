2034b54f3980e8a600e5e0fceac68c485b88958d
milligang <144384537+milligang@users.noreply.github.com>
2023-12-03 19:47:41 +0000

    Sun, Dec 3, 2023, 2:47 PM -05:00

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-# TODO - need to make the .db first
+db = SQL("sqlite:///clue.db")
 
 #TODO: create sql tables
 
