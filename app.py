53f4257f6b64cc8f7e98b9688de538c29b024bbd
milligang <144384537+milligang@users.noreply.github.com>
2023-12-03 19:20:19 +0000

    Sun, Dec 3, 2023, 2:20 PM -05:00

--- app.py
+++ app.py
@@ -25,36 +25,50 @@ def after_request(response):
 
 @app.route("/start", methods=["GET", "POST"])
 def start():
-    """
-    post = both players press start when ready
+    if request.method == "POST":
+        return render_template("homepage.html")
+    '''both players press start when ready
         generate cards for the players
             assign user id to each card
             assign id to winning card set
-        redirect to game board
+        redirect to game board'''
 
-    get = render.template(whatever the home page is called)
-    """
+    else:
+        return render_template("homepage.html")
 
 @app.route("/game_board", methods=["GET", "POST"])
 def game_board():
+    if request.method == "POST":
+        return render_template("homepage.html")
+    else:
+        return render_template("gameboard.html")
     """
     post = player clicks on one of the locations
         flash the location (so that the next page can use it as input)
         redirect(page where player inputs their guess)
 
-    get = render.template(the main game board page)
+    else:
+        return render_template("game_board.html")
     """
 
 @app.route("/guess", methods=["GET", "POST"])
 def guess():
+    if request.method == "POST":
+        return render_template("homepage.html")
+    else:
+        return render_template("guess.html")
     """
     post =
 
     get = render.template(the page where the player can select the suspect, room, and weapon)
     """
 
-@app.route("reveal_cards", methods=["GET", "POST"])
+@app.route("/reveal_cards", methods=["GET", "POST"])
 def reveal_cards():
+    if request.method == "POST":
+        return render_template("homepage.html")
+    else:
+        return render_template("homepage.html")
     """
     post = player selects card from the dropdown and clicks submit
         card = request.form.get(the item selected from the dropdown)
@@ -63,8 +77,12 @@ def reveal_cards():
     get = render.template(the page where the player chooses which card to show)
     """
 
-@app.route("show_cards")
+@app.route("/show_cards")
 def show_cards():
+    if request.method == "POST":
+        return render_template("homepage.html")
+    else:
+        return render_template("mycards.html")
     """
     cards = select * from cards where player_id = ?, current_player
     render.template(page of player's cards, cards = cards)
