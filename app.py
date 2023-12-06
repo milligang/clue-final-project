9d71b9bf2cd7035ce9920fe9712beb4fd0018180
milligang <144384537+milligang@users.noreply.github.com>
2023-12-06 18:23:32 +0000

    Wed, Dec 6, 2023, 1:23 PM -05:00

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from cs50 import SQL
-from flask import Flask, flash, redirect, render_template, request, session, url_for
+from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 
 # Configure application
