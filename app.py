bda530cdc50f042a13970d3cc0da802b92f573ff
milligang <144384537+milligang@users.noreply.github.com>
2023-12-06 18:12:03 +0000

    Wed, Dec 6, 2023, 1:12 PM -05:00

--- app.py
+++ app.py
@@ -124,8 +124,9 @@ def revealcards():
 @app.route("/mycards", methods=["GET", "POST"])
 def mycards():
     if request.method == "POST":
-        
-    return render_template("mycards.html", cards = db.execute("SELECT * FROM cards"), current_player = session["current_player"])
+        redirect("/gameboard2")
+    else:
+        return render_template("mycards.html", cards = db.execute("SELECT * FROM cards"), current_player = session["current_player"])
 
 @app.route("/allcards")
 def allcards():
